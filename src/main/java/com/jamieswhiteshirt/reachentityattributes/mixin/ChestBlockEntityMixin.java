8029ff5a65cdd3f06463af7583326b61fad9ab07
Chloe Dawn <chloe@sapphic.dev>
2021-02-13 20:30:19 +0000

    Make chest open counter mixin less intrusive

--- src/main/java/com/jamieswhiteshirt/reachentityattributes/mixin/ChestBlockEntityMixin.java
+++ src/main/java/com/jamieswhiteshirt/reachentityattributes/mixin/ChestBlockEntityMixin.java
@@ -4,36 +4,33 @@ import com.jamieswhiteshirt.reachentityattributes.ReachEntityAttributes;
 import net.minecraft.block.entity.ChestBlockEntity;
 import net.minecraft.block.entity.LockableContainerBlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.DoubleInventory;
-import net.minecraft.inventory.Inventory;
-import net.minecraft.screen.GenericContainerScreenHandler;
 import net.minecraft.util.math.Box;
 import net.minecraft.world.World;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.At.Shift;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mixin(ChestBlockEntity.class)
 abstract class ChestBlockEntityMixin {
-    @Inject(
+    @ModifyVariable(
         method = "countViewers(Lnet/minecraft/world/World;Lnet/minecraft/block/entity/LockableContainerBlockEntity;III)I",
-        require = 1, allow = 1, at = @At("HEAD"), cancellable = true)
-    private static void countViewersAccountingForReach(final World world, final LockableContainerBlockEntity be, final int x, final int y, final int z, final CallbackInfoReturnable<Integer> info) {
-        int count = 0;
+        require = 1, allow = 1,
+        at = @At(
+            shift = Shift.BEFORE, value = "INVOKE", opcode = Opcodes.INVOKEVIRTUAL,
+            target = "Ljava/util/List;iterator()Ljava/util/Iterator;"))
+    private static List<PlayerEntity> getPlayersAccountingForReach(final List<PlayerEntity> players, final World world, final LockableContainerBlockEntity inventory, final int x, final int y, final int z) {
+        final List<PlayerEntity> actualPlayers = new ArrayList<>(0);
         for (final PlayerEntity player : world.getPlayers()) {
-            if (player.currentScreenHandler instanceof GenericContainerScreenHandler) {
-                final Inventory inv = ((GenericContainerScreenHandler) player.currentScreenHandler).getInventory();
-                if ((inv == be) || ((inv instanceof DoubleInventory) && ((DoubleInventory) inv).isPart(be))) {
-                    final double r = ReachEntityAttributes.getReachDistance(player, 5.0);
-                    if (player.getBoundingBox().intersects(new Box(x - r, y - r, z - r, x + r, y + r, z + r))) {
-                        ++count;
-                    }
-                }
+            final double r = ReachEntityAttributes.getReachDistance(player, 5.0);
+            if (player.getBoundingBox().intersects(new Box(x - r, y - r, z - r, x + r, y + r, z + r))) {
+                actualPlayers.add(player);
             }
         }
-        if (count > 0) {
-            info.setReturnValue(count);
-        }
+        return actualPlayers;
     }
 }
