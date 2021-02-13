925b839a0aa45b542665e2a1282520615fbe6d70
Chloe Dawn <chloe@sapphic.dev>
2021-02-13 19:56:39 +0000

    Fix item interaction raytracing In vanilla this accounts for boat placement, bucketing, ender eye placement, glass bottling, lily pad placement, and spawn egg summoning

---
+++ src/main/java/com/jamieswhiteshirt/reachentityattributes/mixin/ItemMixin.java
@@ -0,0 +1,19 @@
+package com.jamieswhiteshirt.reachentityattributes.mixin;
+
+import com.jamieswhiteshirt.reachentityattributes.ReachEntityAttributes;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(Item.class)
+abstract class ItemMixin {
+    @ModifyConstant(
+        method = "raycast(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/world/RaycastContext$FluidHandling;)Lnet/minecraft/util/hit/BlockHitResult;",
+        require = 4, allow = 4, constant = @Constant(doubleValue = 5.0))
+    private static double getActualReachDistance(final double reachDistance, final World world, final PlayerEntity player) {
+        return ReachEntityAttributes.getReachDistance(player, reachDistance);
+    }
+}
