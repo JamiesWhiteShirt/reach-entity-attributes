8be30fe680a5378f43420870c8640b712c0ba119
Chloe Dawn <chloe@sapphic.dev>
2021-02-13 20:51:24 +0000

    Remove currently unused logger

--- src/main/java/com/jamieswhiteshirt/reachentityattributes/mixin/client/ClientPlayerInteractionManagerMixin.java
+++ src/main/java/com/jamieswhiteshirt/reachentityattributes/mixin/client/ClientPlayerInteractionManagerMixin.java
@@ -3,20 +3,14 @@ package com.jamieswhiteshirt.reachentityattributes.mixin.client;
 import com.jamieswhiteshirt.reachentityattributes.ReachEntityAttributes;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(ClientPlayerInteractionManager.class)
 abstract class ClientPlayerInteractionManagerMixin {
-    @Unique
-    private static final Logger LOGGER = LogManager.getLogger("reach-entity-attributes");
-
     @Shadow @Final private MinecraftClient client;
 
     @ModifyConstant(
