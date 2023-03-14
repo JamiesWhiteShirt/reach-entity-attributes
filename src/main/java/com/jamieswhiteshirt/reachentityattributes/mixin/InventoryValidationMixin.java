a471bae42d0960e75661c05e7b1a11a1c1b3de0e
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-03-15 09:29:19 +1100

    Initial 1.19.4 port

--- src/main/java/com/jamieswhiteshirt/reachentityattributes/mixin/InventoryValidationMixin.java
+++ src/main/java/com/jamieswhiteshirt/reachentityattributes/mixin/InventoryValidationMixin.java
@@ -1,29 +1,20 @@
 package com.jamieswhiteshirt.reachentityattributes.mixin;
 
 import com.jamieswhiteshirt.reachentityattributes.ReachEntityAttributes;
-import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
-import net.minecraft.block.entity.BrewingStandBlockEntity;
-import net.minecraft.block.entity.LootableContainerBlockEntity;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = {
-    AbstractFurnaceBlockEntity.class,
-    BrewingStandBlockEntity.class,
-    LootableContainerBlockEntity.class,
-    PlayerInventory.class
-}, targets = {
-    "net.minecraft.block.entity.LecternBlockEntity$1"
-})
-abstract class InventoryValidationMixin implements Inventory {
-    @ModifyConstant(
-        method = "canPlayerUse(Lnet/minecraft/entity/player/PlayerEntity;)Z",
-        require = 1, allow = 1, constant = @Constant(doubleValue = 64.0))
-    private static double getActualReachDistance(final double reachDistance, final PlayerEntity player) {
-        return ReachEntityAttributes.getSquaredReachDistance(player, reachDistance);
+@Mixin(Inventory.class)
+interface InventoryValidationMixin {
+    @Inject(
+        method = "canPlayerUse(Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/entity/player/PlayerEntity;I)Z",
+        at = @At(value = "RETURN", ordinal = 2), require = 1, allow = 1, cancellable = true)
+    private static void getActualReachDistance(final BlockEntity blockEntity, final PlayerEntity player, final int reachDistance, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(player.squaredDistanceTo((double)blockEntity.getPos().getX() + 0.5, (double)blockEntity.getPos().getY() + 0.5, (double)blockEntity.getPos().getZ() + 0.5) <= ReachEntityAttributes.getSquaredReachDistance(player, reachDistance * reachDistance));
     }
 }
